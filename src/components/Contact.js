08dc2035fecc7331df18eba5504f857071a8aa32
Edward Olstrom <eddieolstrom1@gmail.com>
2023-01-31 17:44:28 -0500

    typo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,7 +33,7 @@ const Contact = () => {
             <h3 className="title">Get In Touch</h3>
             <p className="desc">
               If you want to reach out to discuss my projects or you want to say
-              “hello”, please fill out the form below and I will reply you
+              “hello”, please fill out the form below and I will reply to you
               shortly.
             </p>
           </div>
