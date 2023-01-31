83ecc7a0a9bb35ca8ad4971901e732cdb4297192
Edward Olstrom <eddieolstrom1@gmail.com>
2023-01-31 11:56:54 -0500

    starting

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="container">
+        <div className="roww">
+          {/* Main Title */}
+          <div className="resumo_fn_main_title">
+            <h3 className="subtitle">Introduction</h3>
+            <h3 className="title">Systems Engineer</h3>
+            <p className="desc">
+              I'm a Veteran with the United States Coast Guard with a passion
+              for learning and coding.
+            </p>
+            <img src="img/signature.png" alt="image" />
+          </div>
+          {/* /Main Title */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
